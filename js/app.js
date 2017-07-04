27dfc4516ca4a39fd05b535ac66d25c3333d7082
Zorex Salvo <zsalvo@ayannah.com>
2017-07-04 19:13:53 +0800

    Modify the ajax call

--- js/app.js
+++ js/app.js
@@ -1,20 +1,36 @@
 new Vue({
     el: '#app',
     data: {
-        text: 'Wala pang laman hehe',
-        categories: []
+        keyword: '',
+        city_api: 'https://developers.zomato.com/api/v2.1/locations?query=',
+        collections_api: 'https://developers.zomato.com/api/v2.1/search?',
+        city: null,
+        collections: [],
+        restaurants: [],
     },
     methods: {
-        getCategories: function() {
+        searchCity: function() {
             var vue = this;
-            response  = axios({
+            var getCity  = axios({
                 method: 'get',
-                url: 'https://developers.zomato.com/api/v2.1/categories',
+                url: this.city_api + vue.keyword,
                 headers: {
                     'user-key': '1e80753a90af89a2064a61e97e335b86'
                 }
             }).then(function (response) {
-                vue.categories = response.data.categories;
+                var city = response.data.location_suggestions[0];
+                console.log(city);
+                return axios({
+                    method: 'get',
+                    url: vue.collections_api + 'entity_type=' + city.entity_type + '&entity_id=' + city.entity_id,
+                    headers: {
+                        'user-key': '1e80753a90af89a2064a61e97e335b86'
+                    },
+                });
+            })
+            .then(function (response) {
+                console.log(response.data.restaurants);
+                vue.restaurants = response.data.restaurants;
             });
         },
     }
